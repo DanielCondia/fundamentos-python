a14d8a8fe9b87a20691db372c610485932ef08c4
DanielCondia <daniel.condia@gmail.com>
2024-08-24 20:27:59 -0500

    :sparkles: Se agrega archivo con ejemplos de tipo boolean

---
+++ ejemplos-fundamentales-variables/pythonProject/06_boolean.py
@@ -0,0 +1,17 @@
+"""
+Tener en cuenta que en python las variables booleanas comienzan la primera letra
+en mayúscula, True, False
+"""
+
+is_single = True
+print(is_single)
+
+"""
+El operador de negación !, en este caso se puede usar en python con la palabra clave
+not. Con not podemos negar las operaciones
+"""
+
+print(not False)
+print(not True)
+
+print(not is_single)
