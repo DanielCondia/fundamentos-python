6563520e62495440076911134546bdbd3be0f361
DanielCondia <daniel.condia@gmail.com>
2024-09-01 14:15:57 -0500

    :sparkles: Se agregan ejemplos de operadores aritmeticos

---
+++ ejemplos-fundamentales-variables/pythonProject/08_operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+"""
+Los operadores aritméticos son los mismos, pero tiene algunas características especiales cuando los combinamos con
+strings
+"""
+
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2)
+
+# combinado con string se repite tantas veces como lo indiquemos
+print('Hola ' * 3)
+
+# Obtenemos solo la parte entera de una division
+print(10 // 3)
+
+# Exponente o número elevado
+print(2 ** 3)
