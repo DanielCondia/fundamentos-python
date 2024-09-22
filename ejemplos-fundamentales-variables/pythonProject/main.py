d5acdcd1fa84d6becf47f7c190cc57ad692344ad
DanielCondia <daniel.condia@gmail.com>
2024-09-21 21:06:39 -0500

    :sparkles: Se agrega ejemplo de funcion matematica

--- ejemplos-fundamentales-variables/pythonProject/main.py
+++ ejemplos-fundamentales-variables/pythonProject/main.py
@@ -6,3 +6,9 @@ Esto es un comentario de
 varias lineas.
 Podemos usar cuantas lineas queramos
 '''
+
+deuda = 1000000
+interes = 0.028
+cuotas = 12
+valor_mensual = deuda*(interes*(1+interes)**cuotas) / ((1+interes)**cuotas-1)
+print(valor_mensual)
